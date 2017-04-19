7e9ebc026da0652da70bb1044c34266ad77dbf92
Leung <394869042@qq.com>
2017-04-20 00:08:33 +0800

    first commit

---
+++ html_downloader.py
@@ -0,0 +1,16 @@
+#coding:utf8
+
+from urllib import request
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response=request.urlopen(url)
+
+        if response.getcode()!=200:
+            return None
+
+        return response.read()
